9cbd34251bc686e199036627310b50b510ada8cb
tsukers <melmelovert@gmail.com>
2024-03-27 21:34:52 -0400

    Initial commit

---
+++ src/main/java/net/tsukers/wisteria/WisteriaClient.java
@@ -0,0 +1,10 @@
+package net.tsukers.wisteria;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WisteriaClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
